22b707da3a7e82b808e4e5bd4cbc3d9f968c8941
X4TKC <tovilaskevin@gmail.com>
2025-07-23 20:31:48 -0400

    Add terraform implementation

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,82 @@
-# In this file put all the logic to crete the proper infraestructure
 terraform {
     required_providers {
-        # Add the provideres according to the challenges
+        aws = {
+            source  = "hashicorp/aws"
+            version = "~> 6.0"
+        }
+    }
+    backend "s3"{
+        bucket         = "fsl-devops-terraform-state"
+        key            = "terraform.tfstate"
+        region         = "us-east-1"
     }
 }
+provider "aws" {
+  region = "us-east-1"
+}
+resource "aws_s3_bucket" "website" {
+  bucket = "fsl-devops-${var.environment}-website-bucket"
+}
+resource "aws_s3_bucket" "logs" {
+  bucket = "fsl-devops-${var.environment}-logs-bucket"
+}
+resource "aws_cloudfront_origin_access_control" "website_oac" {
+  name                              = "fsl-oac-${var.environment}"
+  description                       = "oac for ${var.environment} environment"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+resource "aws_cloudfront_distribution" "website" {
+  origin {
+    domain_name              = aws_s3_bucket.website.bucket_regional_domain_name
+    origin_access_control_id = aws_cloudfront_origin_access_control.website_oac.id
+    origin_id                = "s3-${aws_s3_bucket.website.id}"
+  }
 
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "FSL DEVOPS"
+  default_root_object = "index.html"
 
-# Add the resources relatedo to the provider
+  logging_config {
+    include_cookies = false
+    bucket          = aws_s3_bucket.logs.bucket_regional_domain_name
+    prefix          = "cloudfront-logs/${var.environment}/"
+  }
+
+  default_cache_behavior {
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "s3-${aws_s3_bucket.website.id}"
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  price_class = "PriceClass_200"
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  tags = {
+    Environment = var.environment
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
